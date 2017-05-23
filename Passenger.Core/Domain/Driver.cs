036747da07f4d08e2a062149af1832af1e2156df
krissaspdev <krissaspdev@hotmail.com>
2017-05-23 22:36:44 +0200

    Code review

--- Passenger.Core/Domain/Driver.cs
+++ Passenger.Core/Domain/Driver.cs
@@ -5,14 +5,14 @@ namespace Passenger.Core.Domain
 {
     public class Driver
     {
-        public Guid Id { get; protected set; }
-
         public Guid UserId { get; protected set; }
-
         public Vehicle Vehicle { get; protected set; }
-
         public IEnumerable<Route> Routes { get; protected set; }
-
         public IEnumerable<DailyRoute> DailyRoutes { get; protected set; }
+
+        public Driver(Guid userId)
+        {
+            UserId = UserId;
+        }
     }
 }
